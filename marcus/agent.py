c73715af4ffb9e18f11e68ca573af68051688552
sbangariGIT <bangarisairohan@gmail.com>
2024-09-07 20:42:55 -0700

    save version

--- marcus/agent.py
+++ marcus/agent.py
@@ -1,70 +1,112 @@
-# from langchain_core.messages import HumanMessage, SystemMessage, RemoveMessage
-# from langgraph.graph import MessagesState
-# from typing import TypedDict
-# from langchain_anthropic import ChatAnthropic
-# from langchain_core.output_parsers import StrOutputParser
-# from langgraph.graph import StateGraph, START, END
-# from langchain_core.prompts import PromptTemplate
-# from langgraph.prebuilt import ToolNode
+from langchain_core.messages import HumanMessage, SystemMessage, RemoveMessage
+from langgraph.graph import MessagesState
+from typing import TypedDict
+from langchain_anthropic import ChatAnthropic
+from langchain_core.output_parsers import StrOutputParser
+from langgraph.graph import StateGraph, START, END
+from langchain_core.prompts import PromptTemplate
+from langgraph.prebuilt import tools_condition, ToolNode
+from typing import Literal
 
-# # We will use this model for both the conversation and the summarization
-# from langchain_openai import ChatOpenAI
-# model = ChatOpenAI(model="gpt-4o", temperature=0)
-# tools = [add, multiply, divide]
-# llm = ChatOpenAI(model="gpt-4o")
-# llm_with_tools = llm.bind_tools(tools)
+# We will use this model for both the conversation and the summarization
+from langchain_openai import ChatOpenAI
+model = ChatOpenAI(model="gpt-4o", temperature=0)
+def add(a: int, b: int) -> int:
+    """Adds a and b.
 
-# class InputState(TypedDict):
-#     question: str
-#     chat_history: str
+    Args:
+        a: first int
+        b: second int
+    """
+    return a + b
 
-# class OutputState(TypedDict):
-#     answer: str
+def multiply(a: int, b: int) -> int:
+    """Multiplies a and b.
 
-# class PrivateState(TypedDict):
-#     next_node: str
-#     question: str
-#     chat_history: str
+    Args:
+        a: first int
+        b: second int
+    """
+    return a * b
 
-# def get_router_template():
-#     return PromptTemplate.from_template(
-#             """
-#             You are an AI Assistant that is specialized in research of the real estate market of Hyderabad, India. Your Name is MARCUS created by clear-estate.com.
-#             Given the user current question and chat history below, classify which agent is most appropriate to best help with this
-#             `analysis`: Any queries to calculate and solve real estate problems like calculating land/property valuations of the users. Calculate mean, median and any statistical problems related to real estate.
-#             `insights`: Any queries related to finding the current rates, trends, ROI and releated questions regarding current and past real estate questions.
-#             `property`: Any queries related to finding a property, trying to find a place with specific requirements. Direct any property search or place search questions here.
-#             `generic`:  Any queries not related to real estate, about MARCUS which is the product and anything that does not fit the above.
-#             Do not respond with more than one word.
-#             <question>
-#             {input}
-#             </question>
-#             <histroy>
-#             {chat_history}
-#             </history>
+def divide(a: int, b: int) -> float:
+    """Adds a and b.
 
-#         Classification:"""
-#         )
+    Args:
+        a: first int
+        b: second int
+    """
+    return a / b
 
-# def router(state: InputState) -> PrivateState:
-#    chain = (
-#             get_router_template()
-#             | ChatAnthropic(model_name="claude-3-haiku-20240307")
-#             | StrOutputParser()
-#         )
-#    return {"next_node": chain.invoke({"input": state["question"], "chat_history": state["chat_history"]}), "question": state["question"], "chat_history": state["chat_history"]}
+tools = []
+llm = ChatOpenAI(model="gpt-4o")
+tools = [add, multiply, divide]
+llm_with_tools = llm.bind_tools(tools)
 
-# # System message
-# sys_msg = SystemMessage(content="You are a helpful assistant tasked with performing arithmetic on a set of inputs.")
 
-# # Node
-# def property_assistant(state: PrivateState):
-#    return {"messages": [llm_with_tools.invoke([sys_msg] + state["messages"])]}
+class State(MessagesState):
+    next_node: str
 
-# # Define a new graph
-# workflow = StateGraph(input=InputState, output=OutputState)
-# workflow.add_node("marcus_router", router)
-# workflow.add_node("property_search_agent", property_assistant)
+def get_router_template():
+    return PromptTemplate.from_template(
+            """
+            You are an AI Assistant that is specialized in research of the real estate market of Hyderabad, India. Your Name is MARCUS created by clear-estate.com.
+            Given the user current question and chat history below, classify which agent is most appropriate to best help with this
+            `analysis`: Any queries to calculate and solve real estate problems like calculating land/property valuations of the users. Calculate mean, median and any statistical problems related to real estate.
+            `insights`: Any queries related to finding the current rates, trends, ROI and releated questions regarding current and past real estate questions.
+            `property`: Any queries related to finding a property, trying to find a place with specific requirements. Direct any property search or place search questions here.
+            `generic`:  Any queries not related to real estate, about MARCUS which is the product and anything that does not fit the above.
+            Do not respond with more than one word.
+            <question>
+            {input}
+            </question>
+            <histroy>
+            {chat_history}
+            </history>
 
-# # Compile
-# graph = workflow.compile()
+        Classification:"""
+        )
+
+def router(state: State):
+   chain = (
+            get_router_template()
+            | ChatAnthropic(model_name="claude-3-haiku-20240307")
+            | StrOutputParser()
+        )
+   return {"next_node": chain.invoke({"input": state["messages"][-1], "chat_history": state["messages"]})}
+
+# System message
+sys_msg = SystemMessage(content="You are a helpful assistant answer the questions")
+
+# Node
+def property_assistant(state):
+   return {"messages": [llm_with_tools.invoke([sys_msg] + state["messages"])]}
+
+
+def sub_agent_condition(state) -> Literal['property_search_agent', 'insights_search_agent', '__end__']:
+    if state["next_node"] == 'property':
+        return 'property_search_agent'
+    elif state["next_node"] == 'insights':
+        return 'insights_search_agent'
+    else:
+        return '__end__'
+
+# Define a new graph
+workflow = StateGraph(State)
+workflow.add_node("marcus_router", router)
+workflow.add_node("property_search_agent", property_assistant)
+workflow.add_node("insights_search_agent", property_assistant)
+workflow.add_node("tools", ToolNode(tools))
+
+
+# add edges
+workflow.add_edge(START, "marcus_router")
+workflow.add_conditional_edges("marcus_router", sub_agent_condition)
+workflow.add_conditional_edges(
+    "property_search_agent",
+    tools_condition,
+)
+workflow.add_edge("tools", "property_search_agent")
+workflow.add_edge("property_search_agent", END)
+# Compile
+graph = workflow.compile()
--- module-1/studio/agent.py
+++ module-1/studio/agent.py
@@ -1,9 +1,16 @@
-from langchain_core.messages import SystemMessage
-from langchain_openai import ChatOpenAI
-
-from langgraph.graph import START, StateGraph, MessagesState
+from langchain_core.messages import HumanMessage, SystemMessage, RemoveMessage
+from langgraph.graph import MessagesState
+from typing import TypedDict
+from langchain_anthropic import ChatAnthropic
+from langchain_core.output_parsers import StrOutputParser
+from langgraph.graph import StateGraph, START, END
+from langchain_core.prompts import PromptTemplate
 from langgraph.prebuilt import tools_condition, ToolNode
+from typing import Literal
 
+# We will use this model for both the conversation and the summarization
+from langchain_openai import ChatOpenAI
+model = ChatOpenAI(model="gpt-4o", temperature=0)
 def add(a: int, b: int) -> int:
     """Adds a and b.
 
@@ -31,31 +38,70 @@ def divide(a: int, b: int) -> float:
     """
     return a / b
 
-tools = [add, multiply, divide]
-
-# Define LLM with bound tools
+tools = []
 llm = ChatOpenAI(model="gpt-4o")
+tools = [add, multiply, divide]
 llm_with_tools = llm.bind_tools(tools)
 
+
+class State(MessagesState):
+    next_node: str
+
+def get_router_template():
+    return PromptTemplate.from_template(
+            """
+            You are an AI Assistant that is specialized in research of the real estate market of Hyderabad, India. Your Name is MARCUS created by clear-estate.com.
+            Given the user current question and chat history below, classify which agent is most appropriate to best help with this
+            `analysis`: Any queries to calculate and solve real estate problems like calculating land/property valuations of the users. Calculate mean, median and any statistical problems related to real estate.
+            `insights`: Any queries related to finding the current rates, trends, ROI and releated questions regarding current and past real estate questions.
+            `property`: Any queries related to finding a property, trying to find a place with specific requirements. Direct any property search or place search questions here.
+            `generic`:  Any queries not related to real estate, about MARCUS which is the product and anything that does not fit the above.
+            Do not respond with more than one word.
+            <question>
+            {input}
+            </question>
+            <histroy>
+            {chat_history}
+            </history>
+
+        Classification:"""
+        )
+
+def router(state: State):
+   chain = (
+            get_router_template()
+            | ChatAnthropic(model_name="claude-3-haiku-20240307")
+            | StrOutputParser()
+        )
+   return {"next_node": chain.invoke({"input": state["messages"][-1], "chat_history": state["messages"]})}
+
 # System message
-sys_msg = SystemMessage(content="You are a helpful assistant tasked with writing performing arithmetic on a set of inputs.")
+sys_msg = SystemMessage(content="You are a helpful assistant answer the questions")
 
 # Node
-def assistant(state: MessagesState):
+def property_assistant(state):
    return {"messages": [llm_with_tools.invoke([sys_msg] + state["messages"])]}
 
-# Build graph
-builder = StateGraph(MessagesState)
-builder.add_node("assistant", assistant)
-builder.add_node("tools", ToolNode(tools))
-builder.add_edge(START, "assistant")
-builder.add_conditional_edges(
-    "assistant",
-    # If the latest message (result) from assistant is a tool call -> tools_condition routes to tools
-    # If the latest message (result) from assistant is a not a tool call -> tools_condition routes to END
-    tools_condition,
-)
-builder.add_edge("tools", "assistant")
 
-# Compile graph
-graph = builder.compile()
+def sub_agent_condition(state) -> Literal['insights_search_agent', 'property_search_agent', '__end__']:
+    if state["next_node"] == 'property':
+        return 'property_search_agent'
+    elif state["next_node"] == 'insights':
+        return 'insights_search_agent'
+    else:
+        return '__end__'
+
+# Define a new graph
+workflow = StateGraph(State)
+workflow.add_node("marcus_router", router)
+workflow.add_node("insights_search_agent", property_assistant)
+workflow.add_node("property_search_agent", property_assistant)
+
+
+# add edges
+workflow.add_edge(START, "marcus_router")
+workflow.add_conditional_edges("marcus_router", sub_agent_condition)
+workflow.add_edge("insights_search_agent", END)
+workflow.add_edge("property_search_agent", END)
+# Compile
+graph = workflow.compile()
